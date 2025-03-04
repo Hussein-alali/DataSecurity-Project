e1735acb3b38116e114953b99bb2f7dc4fd4c388
seifemad7 <semadam7557@gmail.com>
2025-03-05 01:39:15 +0200

    caesar testing

--- securitylibrary/MainAlgorithms/Ceaser.cs
+++ securitylibrary/MainAlgorithms/Ceaser.cs
@@ -4,24 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SecurityLibrary
 {
     public class Ceaser : ICryptographicTechnique<string, int>
     {
-        public string Encrypt(string plainText, int key)
+
+        // throw new NotImplementedException();
+        public string Encrypt(string ourinput, int shift)
         {
-            throw new NotImplementedException();
-            
+            string encrypted = "";
+            foreach (char character in ourinput)
+            {
+                if (char.IsLetter(character))
+                {
+                    char baseChar = char.IsUpper(character) ? 'A' : 'a';
+                    encrypted += (char)(((character - baseChar + shift) % 26) + baseChar);
+                }
+                else
+                {
+                    encrypted += character;
+                }
+            }
+            return encrypted;
         }
+
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+          //  throw new NotImplementedException();
+            return Encrypt(cipherText, 26 - (key % 26));
         }
 
-        public int Analyse(string plainText, string cipherText)
+        public int Analyse(string plainText, string cipheredText)
         {
-            throw new NotImplementedException();
+          //throw new NotImplementedException();
+            int key = (cipheredText[0] - plainText[0] + 26) % 26;
+            return key;
         }
     }
 }
