fe71b55f9a0c9378ed0882f444c55a841db0dac9
Taher <lTAHERl_2004@outlook.com>
2025-03-04 19:19:47 +0200

    Columnar Encrypt method

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -10,7 +10,7 @@ namespace SecurityLibrary
     {
         public List<int> Analyse(string plainText, string cipherText)
         {
-            Console.Write("Hello");
+            throw new NotImplementedException();
         }
 
         public string Decrypt(string cipherText, List<int> key)
@@ -20,7 +20,46 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            char[] PTchars = plainText.ToCharArray();
+            int width = key.Max();
+            int arrindex = PTchars.Length;
+            while (PTchars.Length % width != 0)
+            {
+                PTchars[arrindex] = 'x';
+                arrindex++;
+            }
+            char[] CTchars = new char[arrindex];
+            int depth = PTchars.Length/ width;
+            char[,] matrix = new char[depth+1, width];
+            int index = 0;
+            for (int i=0;i< depth;i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i,j]= PTchars[index];
+                    index++;
+                }
+            }
+            for (int j = 0; j < width; j++)
+            {
+                matrix[depth, j] =(char)key.ElementAt(j);
+            }
+            int search = 1;
+            int CTindex = 0;
+            for (int j = 0; search < width; j++)
+            {
+                    if (matrix[depth, j % width] == (char)search)
+                    {
+                        for (int i = 0; i < depth; i++)
+                        {
+                            CTchars[CTindex] = matrix[i, j % width];
+                            CTindex++;
+                        }
+                        search++;
+                    }
+            }
+
+                return CTchars.ToString();
         }
     }
 }
