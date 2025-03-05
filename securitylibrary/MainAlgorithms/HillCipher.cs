52fbd114b7c558b7fc892822d58c3eacb2126f46
7oosmoon <bdalrhmnh73m@gmail.com>
2025-03-05 14:39:31 +0200

    Decrypt

--- securitylibrary/MainAlgorithms/HillCipher.cs
+++ securitylibrary/MainAlgorithms/HillCipher.cs
@@ -15,38 +15,91 @@ namespace SecurityLibrary
         {
             throw new NotImplementedException();
         }
+        public int detkeyF(List<int> key)
+        {
+            int detkey =( key[0] * key[3] - key[1] * key[2])%26;
+            if (detkey < 0)
+                detkey += 26;
+            return detkey;
+            //throw new NotImplementedException();
+        }
+        public int adjF(List<int> key,int i ,int j)
+        {
+            List<int> listfornow = new List<int>(4);
 
+            for (int k = 0; k < 3; k++)
+            {
+                for (int i1 = 0; i1 < 3; i1++)
+                {
+                    if (k != i && i1 != j)
+                    {
+                        listfornow.Add(key[k*3 + i1]);
+                    }
+                }
+            }
 
+            int finalnum = detkeyF(listfornow);
+            if ((i + j) % 2!=0)
+            {
+                finalnum *= -1;
+            }
+            finalnum %= 26;
+            if (finalnum < 0)
+                finalnum += 26;
+            return finalnum;
+            //throw new NotImplementedException();
+        }
+        public int detInverse(int det )
+        {
+            det %= 26;
+            if (det < 0) det += 26;
+            for (int x = 1; x < 26; x++)
+            {
+                if ((det * x) % 26 == 1)
+                    return x;
+            }
+            throw new Exception("No det inverse exists.");
+        }
         public List<int> Decrypt(List<int> cipherText, List<int> key)
         {
 
             int Mat = (int)Math.Sqrt(key.Count);
+            int detkey;
+            List<int> InverseKey = new List<int>(new int[key.Count]);
 
             if (Mat ==2)
             {
-                
-
-
-
-
+                detkey = detkeyF(key);
+                detkey=detInverse(detkey);
+                InverseKey[0] = ((key[3] * detkey)%26);
+                  InverseKey[1] =(((key[1]*-1) * detkey) % 26);
+                  InverseKey[2] =(((key[2] * -1) * detkey) % 26);
+                  InverseKey[3] =((key[0] * detkey) % 26);
             }
             else if(Mat == 3)
             {
-                
+                detkey = (key[0] * key[4] * key[8] + key[1] * key[5] * key[6] + key[2] * key[3] * key[7]) - (key[1] * key[3] * key[8] + key[0] * key[5] * key[7] + key[2] * key[4] * key[6]);
+                detkey %= 26;
+                if (detkey < 0)
+                    detkey += 26;
+                detkey= detInverse(detkey);
 
-
-
-
-
-
+                for (int i = 0; i < Mat; i++)
+                {
+                    int value;
+                    for (int j = 0; j < Mat; j++)
+                    {
+                        value = (adjF(key, i, j) * detkey) % 26;
+                        if (value < 0)
+                            value += 26;
+                        InverseKey[j *Mat + i]=value;
+                    }
+                }
             }
 
+            return Encrypt(cipherText, InverseKey);
 
-
-
-
-
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
 
