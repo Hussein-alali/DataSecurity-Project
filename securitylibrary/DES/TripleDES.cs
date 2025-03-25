2a845aafb1dbf2cc9089c5a17379773bf8a0566b
g3fr <g3fr@DESKTOP-SVCENTT>
2025-03-25 23:43:09 +0200

    finaly

--- securitylibrary/DES/TripleDES.cs
+++ securitylibrary/DES/TripleDES.cs
@@ -13,12 +13,24 @@ namespace SecurityLibrary.DES
     {
         public string Decrypt(string cipherText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des1 = new DES();
+            // throw new NotImplementedException();
+            string one = des1.Decrypt(cipherText, key[0]);
+            string two = des1.Encrypt(one, key[1]);
+            string final = des1.Decrypt(two, key[0]);
+
+            return final;
         }
 
         public string Encrypt(string plainText, List<string> key)
         {
-            throw new NotImplementedException();
+            DES des1 = new DES();
+            // throw new NotImplementedException();
+            string one = des1.Encrypt(plainText, key[0]);
+            string two = des1.Decrypt(one, key[1]);
+            string final = des1.Encrypt(two, key[0]);
+
+            return final;
         }
 
         public List<string> Analyse(string plainText,string cipherText)
